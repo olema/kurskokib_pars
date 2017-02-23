6d18c4fa0bac1ac7188b495e11ac36a94126494f
olema <matushkin.oleg@gmail.com>
2017-02-23 13:02:06 +0300

    Некоторая логика перенесена в функции

--- reviews.py
+++ reviews.py
@@ -18,25 +18,47 @@ import sys
 import configparser
 
 
-def main():
-    # Работа с конфигом reviews.config
-    # с помощью модуля configparser
+def config(confname):
+   # Работа с конфигом reviews.config с помощью модуля configparser.
+   # Возвращает словарь с переменными из конфига confname
+   # TODO Добавить проверку существования файла из confname.
+
     config = configparser.ConfigParser()
-    config.read('reviews.con')
+    config.read(confname)
+    dict_var = dict()
     try:
         urls = config['urls']
-        url_admin = urls['url_admin']
-        url_rev = urls['url_rev']
+        dict_var['url_admin'] = urls['url_admin']
+        dict_var['url_rev'] = urls['url_rev']
     except KeyError:
-        #TODO Добавить выдачу ошибки в файл-флаг и на email
+        # TODO Добавить выдачу ошибки в файл-флаг и на email
         print('Error...')
         exit(-1)
+    return dict_var
+
+
+def tagparse(seekstring, namevar):
+    # Ищет в строке seekstring имя namevar вида name="var0" и возвращает строку,
+    # которая содержится в valuevar="xxxx" между кавычками (без кавычек).
+    # Если namevar не найдена - возвращает пустую строку?
+    #   пример: seekstring = '<TD name="var1" value="Иванов"'
+    #           namevar = 'var1'
+    #           Функция должна вернуть 'Иванов'
+    if namevar not in seekstring:
+        return ''
+    else:
+        return 'YEAH!!!!!'
+
+
+def main():
+    # Читаем переменные из файла конфигурации
+    dvar = config('reviews.config')
 
     # Читаем страницу с сайта
     s = requests.session()
     data = {"ln": sys.argv[1], "pd": sys.argv[2]}
-    r = s.post(url_admin, data=data)
-    r = s.get(url_rev)
+    r = s.post(dvar['url_admin'], data=data)
+    r = s.get(dvar['url_rev'])
     print(r.url)
 
     # Оставляем только строки, с отзывами "на модерации"
@@ -48,8 +70,21 @@ def main():
             in_moder_block = True
         if 'Отзывы в базе' in l:
             in_moder_block = False
+            break
         if in_moder_block:
             moder.append(l.strip())
+
+    # DEBUG Для отладки
+    f = open('moder_block.txt', 'w')
+    for l in moder:
+        f.write(l + '\n')
+    f.close()
+
+    # В moder[] - строки из блока "на модерации"
+    print('\n\n****** test function tagparse() *********')
+    print('moder[1] = ', moder[1])
+    print('tagparse(moder[1], "name") = ', tagparse(moder[1], 'name'))
+    print('****** test function tagparse() *********\n\n')
 
     # Формируем список messages с словарями полей var0..var7
     messages = []
