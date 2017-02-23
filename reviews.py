3a430d60ad78c8b24ce51d84b4156ad40147f9d9
olema <matushkin.oleg@gmail.com>
2017-02-23 10:32:53 +0300

    Добавлена работа с конфигурационным файлом

--- reviews.py
+++ reviews.py
@@ -17,55 +17,62 @@ import requests
 import sys
 import configparser
 
-# TODO - работа с конфигом reviews.config
-# с помощью модуля configparser
-config = configparser.ConfigParser()
-config.read('reviews.config')
-print(config.sections())
-urls = config['urls']
-url_admin = urls['url_admin']
-url_rev = urls['url_rev']
-print(url_admin, url_rev)
 
-# Читаем страницу с сайта
-s = requests.session()
-data = {"ln": sys.argv[1], "pd": sys.argv[2]}
-r = s.post(url_admin, data=data)
-r = s.get(url_rev)
-print(r.url)
+def main():
+    # Работа с конфигом reviews.config
+    # с помощью модуля configparser
+    config = configparser.ConfigParser()
+    config.read('reviews.con')
+    try:
+        urls = config['urls']
+        url_admin = urls['url_admin']
+        url_rev = urls['url_rev']
+    except KeyError:
+        #TODO Добавить выдачу ошибки в файл-флаг и на email
+        print('Error...')
+        exit(-1)
 
-# Оставляем только строки, с отзывами "на модерации"
-l = ''
-moder = []
-in_moder_block = False
-for l in r.text.splitlines():
-    if 'Отзывы на модерации' in l:
-        in_moder_block = True
-    if 'Отзывы в базе' in l:
-        in_moder_block = False
-    if in_moder_block:
-        moder.append(l.strip())
+    # Читаем страницу с сайта
+    s = requests.session()
+    data = {"ln": sys.argv[1], "pd": sys.argv[2]}
+    r = s.post(url_admin, data=data)
+    r = s.get(url_rev)
+    print(r.url)
 
-# Формируем список messages с словарями полей var0..var7
-messages = []
-d = dict()
-for l in moder:
-    if 'name="var0"' in l:
-        fv = l.find('value=')
-        vval = l[fv:fv+30]
-        d['var0'] = vval
-    if 'name="var1"' in l:
-        fv = l.find('value=')
-        vval = l[fv:fv+30]
-        d['var1'] = vval
-    if 'name="var2"' in l:
-        fv = l.find('value=')
-        vval = l[fv:fv+30]
-        d['var2'] = vval
-        messages.append(d)
-        d = dict()
+    # Оставляем только строки, с отзывами "на модерации"
+    l = ''
+    moder = []
+    in_moder_block = False
+    for l in r.text.splitlines():
+        if 'Отзывы на модерации' in l:
+            in_moder_block = True
+        if 'Отзывы в базе' in l:
+            in_moder_block = False
+        if in_moder_block:
+            moder.append(l.strip())
 
-for d in messages:
-    for item in d:
-        print(item, ': ', d[item])
+    # Формируем список messages с словарями полей var0..var7
+    messages = []
+    d = dict()
+    for l in moder:
+        if 'name="var0"' in l:
+            fv = l.find('value=')
+            vval = l[fv:fv+30]
+            d['var0'] = vval
+        if 'name="var1"' in l:
+            fv = l.find('value=')
+            vval = l[fv:fv+30]
+            d['var1'] = vval
+        if 'name="var2"' in l:
+            fv = l.find('value=')
+            vval = l[fv:fv+30]
+            d['var2'] = vval
+            messages.append(d)
+            d = dict()
 
+    for d in messages:
+        for item in d:
+            print(item, ': ', d[item])
+
+if __name__ == '__main__':
+    main()
